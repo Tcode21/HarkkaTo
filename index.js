a5216b715e8754bb5b0ab1a8e10282846562ad7d
Your Name <you@example.com>
2021-09-23 13:55:31 +0300

    viides

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('moro')
+const fahrenheitSyote = document.querySelector('input');
+const fahrenheitTuloste = document.querySelector('#fahrenheit');
+const celsiusTuloste = document.querySelector('#celsius');
+const muunnaButton = document.querySelector('button');
+
+function muunna() {
+    let syote = fahrenheitSyote.value;
+    fahrenheitTuloste.textContent = syote;
+
+
+}
+
+
