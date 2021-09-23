69434077abc730b46cd5624b708f12ca4c5ff2e0
Your Name <you@example.com>
2021-09-23 14:28:12 +0300

    syöte tulosteeksi

--- index.js
+++ index.js
@@ -3,11 +3,20 @@ const fahrenheitTuloste = document.querySelector('#fahrenheit');
 const celsiusTuloste = document.querySelector('#celsius');
 const muunnaButton = document.querySelector('button');
 
+function fahrenheitCelsiukseksi(fahrenheit) {
+    let celsius = fahrenheit;
+    return celsius;
+
+
+}
+
+
+
 function muunna() {
     let syote = fahrenheitSyote.value;
     fahrenheitTuloste.textContent = syote;
 
-    celsiusTuloste.textContent = syote;
+    celsiusTuloste.textContent = fahrenheitCelsiukseksi(syote);
 }
 
 
